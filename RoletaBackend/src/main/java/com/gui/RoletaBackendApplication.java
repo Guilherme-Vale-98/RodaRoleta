eaaf9f36bc3b3ec1d1596b8ec2facd98f352f1f8
Guilherme Vale <guilherme.vale98@gmail.com>
2024-01-21 08:03:48 -0300

    first commit

---
+++ RoletaBackend/src/main/java/com/gui/RoletaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoletaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoletaBackendApplication.class, args);
+	}
+
+}
