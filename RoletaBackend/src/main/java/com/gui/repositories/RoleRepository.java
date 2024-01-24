b6e5f643d1d7d47773d4edd6c969230b25f31b95
Guilherme Vale <guilherme.vale98@gmail.com>
2024-01-24 07:08:13 -0300

    Added Role and User Repository

---
+++ RoletaBackend/src/main/java/com/gui/repositories/RoleRepository.java
@@ -0,0 +1,5 @@
+package com.gui.repositories;
+
+public interface RoleRepository {
+
+}
--- RoletaBackend/src/main/java/com/gui/security/JwtUtils.java
+++ RoletaBackend/src/main/java/com/gui/security/JwtUtils.java
@@ -8,7 +8,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
-ar pr
+
+import com.gui.models.UserDetailsImpl;
 
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
