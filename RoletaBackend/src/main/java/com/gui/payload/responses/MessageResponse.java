4a5cdda5dfcb1ec6c9d010f472d7cf809da4c00c
Guilherme Vale <guilherme.vale98@gmail.com>
2024-01-24 22:03:18 -0300

    added requests package

--- RoletaBackend/src/main/java/com/gui/payload/responses/MessageResponse.java
+++ RoletaBackend/src/main/java/com/gui/payload/responses/MessageResponse.java
@@ -1,5 +1,17 @@
 package com.gui.payload.responses;
 
 public class MessageResponse {
+	  private String message;
 
-}
+	  public MessageResponse(String message) {
+	    this.message = message;
+	  }
+
+	  public String getMessage() {
+	    return message;
+	  }
+
+	  public void setMessage(String message) {
+	    this.message = message;
+	  }
+	}
