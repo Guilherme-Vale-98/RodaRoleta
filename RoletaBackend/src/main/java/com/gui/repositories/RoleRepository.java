cffcb3e32a1d46bc4ef6ec4633fb3bd9a97e239c
Guilherme Vale <guilherme.vale98@gmail.com>
2024-01-24 21:56:03 -0300

    Added payload responses

--- RoletaBackend/src/main/java/com/gui/repositories/RoleRepository.java
+++ RoletaBackend/src/main/java/com/gui/repositories/RoleRepository.java
@@ -1,5 +1,12 @@
 package com.gui.repositories;
 
-public interface RoleRepository {
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gui.models.ERole;
+import com.gui.models.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+	Optional<Role> findByName(ERole name);
 }
