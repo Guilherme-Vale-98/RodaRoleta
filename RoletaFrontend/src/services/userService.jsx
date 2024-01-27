ae2873404f304ae706905604c0fc1a9007c87338
Guilherme Vale <guilherme.vale98@gmail.com>
2024-01-27 15:32:26 -0300

    created auth and user service

---
+++ RoletaFrontend/src/services/userService.jsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+import authHeader from "./authHeader";
+
+const API_URL = "http://localhost:8080/test/";
+
+const getPublicContent = () => {
+  return axios.get(API_URL + "all");
+};
+
+const getUserBoard = () => {
+  return axios.get(API_URL + "user", { headers: authHeader() });
+};
+
+const userService = {
+  getPublicContent,
+  getUserBoard,
+  getModeratorBoard,
+  getAdminBoard,
+};
+
+export default userService
