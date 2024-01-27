efe6de6321d2840f36f524074e50215e42e68899
Guilherme Vale <guilherme.vale98@gmail.com>
2024-01-27 14:17:59 -0300

    added slice folder

---
+++ RoletaFrontend/src/slices/sliceAuth.jsx
@@ -0,0 +1,82 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { setMessage } from "./sliceMessage.jsx";
+
+import authService from "../services/authService.jsx";
+
+const user = JSON.parse(localStorage.getItem("user"));
+
+export const register = createAsyncThunk(
+  "auth/register",
+  async ({ username, email, password }, thunkAPI) => {
+    try {
+      const response = await authService.register(username, email, password);
+      thunkAPI.dispatch(setMessage(response.data.message));
+      return response.data;
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      thunkAPI.dispatch(setMessage(message));
+      return thunkAPI.rejectWithValue();
+    }
+  }
+);
+
+export const login = createAsyncThunk(
+  "auth/login",
+  async ({ username, password }, thunkAPI) => {
+    try {
+      const data = await authService.login(username, password);
+      return { user: data };
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      thunkAPI.dispatch(setMessage(message));
+      return thunkAPI.rejectWithValue();
+    }
+  }
+);
+
+export const logout = createAsyncThunk("auth/logout", async () => {
+  await authService.logout();
+});
+
+const initialState = user
+  ? { isLoggedIn: true, user }
+  : { isLoggedIn: false, user: null };
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(register.fulfilled, (state, action) => {
+        state.isLoggedIn = false;
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.isLoggedIn = false;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.isLoggedIn = true;
+        state.user = action.payload.user;
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.isLoggedIn = false;
+        state.user = null;
+      })
+      .addCase(logout.fulfilled, (state, action) => {
+        state.isLoggedIn = false;
+        state.user = null;
+      });
+  },
+});
+
+const { reducer } = authSlice;
+export default reducer;
