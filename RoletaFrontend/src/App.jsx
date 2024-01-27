9735f78867ad3af942e5a55bf907d97a47f3c1c7
Guilherme Vale <guilherme.vale98@gmail.com>
2024-01-27 20:50:57 -0300

    started Form and Home component

--- RoletaFrontend/src/App.jsx
+++ RoletaFrontend/src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './Components/Home'
 
 function App() {
   return (
     <>
-     <div> My app</div>
+     <Home/>
     </>
   )
 }
