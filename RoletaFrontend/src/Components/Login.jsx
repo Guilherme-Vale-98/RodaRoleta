e4d9775fa62b04b3394c470b49c615c4819260e8
Guilherme Vale <guilherme.vale98@gmail.com>
2024-02-03 16:03:19 -0300

    added login page style, and font awesome package

--- RoletaFrontend/src/Components/Login.jsx
+++ RoletaFrontend/src/Components/Login.jsx
@@ -1,102 +1,112 @@
-import React, { useEffect, useState } from 'react'
-import "../app.css"
+import React, { useEffect, useState } from "react";
+import "../app.css";
 import { login } from "../slices/sliceAuth";
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 import * as Yup from "yup";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import { clearMessage } from "../slices/sliceMessage";
-import { Navigate } from 'react-router-dom';
-import App from '../App';
+import { Navigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faLock } from "@fortawesome/free-solid-svg-icons";
+import App from "../App";
+
 const Login = () => {
+  const [loading, setLoading] = useState(false);
 
-    const [loading, setLoading] = useState(false);
+  const { isLoggedIn } = useSelector((state) => state.auth);
+  const { message } = useSelector((state) => state.message);
 
-    const { isLoggedIn } = useSelector((state) => state.auth);
-    const { message } = useSelector((state) => state.message);
-  
-    const dispatch = useDispatch();
-  
-    useEffect(() => {
-      dispatch(clearMessage());
-    }, [dispatch]);
-    
-    const initialValues = {
-      username: "",
-      password: "",
-    };
-  
-    const validationSchema = Yup.object().shape({
-      username: Yup.string().required("Esse campo é obrigatório!"),
-      password: Yup.string().required("Esse campo é obrigatório!"),
-    });
-  
-    const handleLogin = (formValue) => {
-        const { username, password } = formValue;
-        setLoading(true);
-        dispatch(login({ username, password }))
-          .unwrap()
-          .then(() => {
-            navigate("/");
-            window.location.reload();
-          })
-          .catch(() => {
-            setLoading(false);
-          });
-      };
-      if (isLoggedIn) {
-        return <Navigate to="/" />;
-      }
+  const dispatch = useDispatch();
 
-      return (
-        <div className="form">
-          <div className="">
-            <Formik
-              initialValues={initialValues}
-              validationSchema={validationSchema}
-              onSubmit={handleLogin}
-            >
-              <Form>
-                <div className="">
-                  <label htmlFor="username">Nome de usuário</label>
-                  <Field name="username" type="text" className="" />
-                  <ErrorMessage
-                    name="username"
-                    component="div"
-                    className=""
-                  />
-                </div>
-    
-                <div className="">
-                  <label htmlFor="password">Senha</label>
-                  <Field name="password" type="password" className="" />
-                  <ErrorMessage
-                    name="password"
-                    component="div"
-                    className=""
-                  />
-                </div>
-    
-                <div className="">
-                  <button type="submit" className="" disabled={loading}>
-                    {loading && (
-                      <span className=""></span>
-                    )}
-                    <span>Login</span>
-                  </button>
-                </div>
-              </Form>
-            </Formik>
-          </div>
-    
-          {message && (
-            <div className="">
-              <div className="" role="alert">
-                {message}
+  useEffect(() => {
+    dispatch(clearMessage());
+  }, [dispatch]);
+
+  const initialValues = {
+    username: "",
+    password: "",
+  };
+
+  const validationSchema = Yup.object().shape({
+    username: Yup.string().required("Esse campo é obrigatório!"),
+    password: Yup.string().required("Esse campo é obrigatório!"),
+  });
+
+  const handleLogin = (formValue) => {
+    const { username, password } = formValue;
+    setLoading(true);
+    dispatch(login({ username, password }))
+      .unwrap()
+      .then(() => {
+        navigate("/");
+        window.location.reload();
+      })
+      .catch(() => {
+        setLoading(false);
+      });
+  };
+  if (isLoggedIn) {
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <div className="form">
+      <div className="">
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={handleLogin}
+        >
+          <Form className="form-wrapper">
+            <div className="login-title"><p>Faça o seu login!</p></div>
+            <div className="circle"></div>
+            <div className="form-inputs">
+              <div className="icon-wrapper">
+                <FontAwesomeIcon className="icon" icon={faUser} />
               </div>
+              <label htmlFor="username"></label>
+              <Field
+                name="username"
+                type="text"
+                placeholder="Usuário"
+                className=""
+              />
+              <ErrorMessage name="username" component="div" className="" />
             </div>
-          )}
-        </div>
-      );
-}
 
-export default Login
+            <div className="form-inputs">
+              <div className="icon-wrapper">
+                <FontAwesomeIcon className="icon" icon={faLock} />
+              </div>
+              <label htmlFor="password"></label>
+              <Field
+                name="password"
+                placeholder="Senhaaaaaaaaaaa"
+                type="password"
+                className=""
+              />
+              <ErrorMessage name="password" component="div" className="" />
+            </div>
+
+            <div className="button-wrapper">
+              <button type="submit" className="" disabled={loading}>
+                {loading && <span className=""></span>}
+                <span>Login</span>
+              </button>
+            </div>
+          </Form>
+        </Formik>
+      </div>
+
+      {message && (
+        <div className="">
+          <div className="" role="alert">
+            {message}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Login;
