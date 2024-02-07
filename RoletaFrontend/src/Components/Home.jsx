f0026fd127cbf92e9c23b3b2c87d0b48e4fffd1a
Guilherme Vale <guilherme.vale98@gmail.com>
2024-02-07 13:41:32 -0300

    created matchService

--- RoletaFrontend/src/Components/Home.jsx
+++ RoletaFrontend/src/Components/Home.jsx
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../slices/sliceAuth";
 import WordBoard from "./Board/WordBoard";
 import Wheel from "./Wheel/Wheel";
+import userService from "../services/userService";
+import matchService from "../services/MatchService";
 
 const Home = () => {
   const { user: currentUser } = useSelector((state) => state.auth);
@@ -11,8 +13,8 @@ const Home = () => {
 
   const logOut = useCallback(() => {
     dispatch(logout());
+    
   }, [dispatch]);
-
   return (
     <div>
       <button onClick={logOut}>Jogar</button>
