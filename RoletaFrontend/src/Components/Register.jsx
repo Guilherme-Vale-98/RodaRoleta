2cef46816cf8bf983e9f5b6115aa40792cb01ed3
Guilherme Vale <guilherme.vale98@gmail.com>
2024-02-06 22:46:12 -0300

    styled Register page

--- RoletaFrontend/src/Components/Register.jsx
+++ RoletaFrontend/src/Components/Register.jsx
@@ -1,21 +1,23 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import * as Yup from "yup";
+import "../app.css";
 import { Formik, Field, Form, ErrorMessage } from "formik";
-import { clearMessage } from '../slices/sliceMessage';
-import { register } from '../slices/sliceAuth';
+import { clearMessage } from "../slices/sliceMessage";
+import { register } from "../slices/sliceAuth";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faLock, faUser } from "@fortawesome/free-solid-svg-icons";
 
 const Register = () => {
   const [successful, setSuccessful] = useState(false);
 
-  const { message } = useSelector((state)=>state.message);
+  const { message } = useSelector((state) => state.message);
 
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(clearMessage());
   }, [dispatch]);
-
 
   const initialValues = {
     username: "",
@@ -24,31 +26,28 @@ const Register = () => {
   };
 
   const validationSchema = Yup.object().shape({
-    username: Yup.string().test(
-      "len",
-      "O nome precisa ter entre 3 e 20 caracteres.",
-      (val) =>
-        val &&
-        val.toString().length >= 3 &&
-        val.toString().length <= 20
-    ).required("Esse campo é obrigatório!"),
-    email: Yup.string().email("Este não é um email válido.")
-    .required("Esse campo é obrigatório!"),
+    username: Yup.string()
+      .test(
+        "len",
+        "O nome precisa ter entre 3 e 20 caracteres.",
+        (val) =>
+          val && val.toString().length >= 3 && val.toString().length <= 20
+      )
+      .required("Esse campo é obrigatório!"),
+    email: Yup.string()
+      .email("Este não é um email válido.")
+      .required("Esse campo é obrigatório!"),
     password: Yup.string()
-    .test(
-      "len",
-      "A senha deve conter no mínimo 4 caracteres",
-      (val) =>
-        val &&
-        val.toString().length >= 4
-    )
-    .required("Esse campo é obrigatório!"),
-  })
+      .test(
+        "len",
+        "A senha deve conter no mínimo 4 caracteres",
+        (val) => val && val.toString().length >= 4
+      )
+      .required("Esse campo é obrigatório!"),
+  });
 
-  
   const handleRegister = (formValue) => {
     const { username, email, password } = formValue;
-
     setSuccessful(false);
     dispatch(register({ username, email, password }))
       .unwrap()
@@ -61,56 +60,75 @@ const Register = () => {
   };
 
   return (
-    <div className='register-form'>
-      <Formik
+    <div className="form">
+      <div>
+        <Formik
           initialValues={initialValues}
           validationSchema={validationSchema}
           onSubmit={handleRegister}
         >
-          <Form>
+          <Form className="form-wrapper">
             {!successful && (
-              <div>
-                <div className="form-group">
-                  <label htmlFor="username">Nome de usuário</label>
-                  <Field name="username" type="text" />
+              <>
+                <div className="login-title">
+                  <p>Registre-se para jogar!</p>
+                </div>
+                <div className="register-circle"></div>
+                <div className="form-inputs">
+                  <div className="icon-wrapper">
+                    <FontAwesomeIcon className="icon" icon={faUser} />
+                  </div>
+                  <label htmlFor="username"></label>
+                  <Field name="username" placeholder="Usuário" type="text" />
                   <ErrorMessage
                     name="username"
+                    className="error"
                     component="div"
                   />
                 </div>
-
-                <div className="form-group">
-                  <label htmlFor="email">Email</label>
-                  <Field name="email" type="email"  />
+                
+                <div className="form-inputs">
+                  <div className="icon-wrapper">
+                    <FontAwesomeIcon className="icon" icon={faEnvelope} />
+                  </div>
+                  <label htmlFor="email"></label>
+                  <Field name="email" placeholder="Email" type="email" />
                   <ErrorMessage
                     name="email"
+                    className="error"
                     component="div"
                   />
                 </div>
 
-                <div className="form-group">
-                  <label htmlFor="password">Password</label>
-                  <Field
-                    name="password"
-                    type="password"
-                  />
+                <div className="form-inputs">
+                  <div className="icon-wrapper">
+                    <FontAwesomeIcon className="icon" icon={faLock} />
+                  </div>
+                  <label htmlFor="password"></label>
+                  <Field name="password" placeholder="Senha" type="password" />
                   <ErrorMessage
                     name="password"
+                    className="error"
                     component="div"
                   />
                 </div>
 
-                <div className="form-group">
-                  <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+                <div className="button-wrapper">
+                  <button type="submit" className="btn btn-primary btn-block">
+                    Registrar!
+                  </button>
                 </div>
-              </div>
+              </>
             )}
           </Form>
         </Formik>
-        {message && (
-        <div >
+      </div>
+      {message && (
+        <div>
           <div
-            className={successful ? "alert alert-success" : "alert alert-danger"}
+            className={
+              successful ? "alert alert-success" : "alert alert-danger"
+            }
             role="alert"
           >
             {message}
@@ -118,7 +136,7 @@ const Register = () => {
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
