4617780ffe8a5c37319effe6c588347c15e539d0
Guilherme Vale <guilherme.vale98@gmail.com>
2024-02-18 02:11:43 -0300

    Worked game animation

--- RoletaFrontend/src/Components/PlayerCard/PlayerCard.jsx
+++ RoletaFrontend/src/Components/PlayerCard/PlayerCard.jsx
@@ -1,36 +1,51 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './PlayerCard.css'
 import { motion, useAnimation } from "framer-motion";
+import { useSelector } from 'react-redux';
 
 
-const PlayerCard = ({spinResult, matchScore, setPlayerLetter, playerLetterArray}) => {
+
+const PlayerCard = ({spinResult, matchScore, setPlayerLetter, playerLetterArray, setSpinResult}) => {
   const [chosenLetter, setChosenLetter] = useState('');
+  const { user: currentUser } = useSelector((state) => state.auth);
   const handleChange = (event) => {
     setChosenLetter(event.target.value);
   };
-  
+
   const [isActive, setIsActive] = useState(false);
   const controls = useAnimation();
 
   const toggleAnimation = () => {
     setIsActive(!isActive);
-    controls.start({ y: isActive ? 0 : 0 });
+    setTimeout(() => {
+      controls.start({ y: isActive ? 0 : 250 });
+    }, isActive ? 3 * 1000 : 9 * 1000);
   };
+
+  useEffect(()=>{
+    if(spinResult){
+      toggleAnimation();
+    }    
+  }, [spinResult])
+
   const chooseClickHandler = () => {
       if(!chosenLetter){
         return
-      }  
+      }
+      if(playerLetterArray.includes(chosenLetter.toLocaleUpperCase())){
+        return
+      } 
     setPlayerLetter(chosenLetter.toLocaleUpperCase());
+    toggleAnimation();
     setChosenLetter('');
   }
 
   return (
     <motion.div className="card-container"
       animate={controls}
-      transition={{ duration: 1, ease: "easeInOut" }}
-      onClick={toggleAnimation}
+      transition={{ duration: 1, ease: "easeInOut"}}
     >
-      <p>Player: </p>
+      <p>Player: {currentUser.username}</p>
       <div>Score: {matchScore}</div>
       <div>Letras escolhidas: {playerLetterArray}</div>
       <div>Escolha uma letra por: {spinResult}
--- RoletaFrontend/src/Components/Wheel/Wheel.jsx
+++ RoletaFrontend/src/Components/Wheel/Wheel.jsx
@@ -1,21 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { motion, useAnimation } from "framer-motion";
 import wheel from "../../assets/wheel.png";
 import "./Wheel.css";
 
-const Wheel = ({setSpinResult}) => {
+const Wheel = ({setSpinResult, playerLetter }) => {
   const [rotationDegree, setRotationDegree] = useState(0);
-  
-
-
   const [isActive, setIsActive] = useState(false);
   const controls = useAnimation();
 
   const toggleAnimation = () => {
     setIsActive(!isActive);
-    controls.start({ x: isActive ? 0 : 900 });
+    
+    setTimeout(()=>{
+      controls.start({ x: isActive ? 0 : 900 });
+    }, isActive ? 3 * 1000 : 9 * 1000);
   };
 
+  useEffect(()=>{
+    if(playerLetter){
+      toggleAnimation();
+    } 
+  }, [playerLetter])
 
   const wheelValues = [
     "perdeu",
@@ -50,6 +55,7 @@ const Wheel = ({setSpinResult}) => {
     setRotationDegree((prevRotationdegree) => {
       const nextRotationDegree = prevRotationdegree + 15 * randomIndex + 360;
       setSpinResult(wheelValues[Math.floor(nextRotationDegree / 15) % 24]);
+      toggleAnimation();
       return nextRotationDegree;
     });
   };
@@ -59,8 +65,7 @@ const Wheel = ({setSpinResult}) => {
       <motion.div className="wheel-container toLeft"
       initial={{ x: 0 }}
       animate={controls}
-      transition={{ duration: 1, ease: "easeInOut" }}
-      onClick={toggleAnimation}
+      transition={{ duration: 1, ease: "easeInOut"}}
       >
         <div className="picker"></div>
         <motion.img
