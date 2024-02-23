c1cd672b7ac278d0256ec50bf5b8af8772d57ff7
Guilherme Vale <guilherme.vale98@gmail.com>
2024-02-23 01:23:14 -0300

    fix animation freezing bugs

--- RoletaFrontend/src/Components/PlayerCard/PlayerCard.jsx
+++ RoletaFrontend/src/Components/PlayerCard/PlayerCard.jsx
@@ -5,7 +5,7 @@ import { useSelector } from 'react-redux';
 
 
 
-const PlayerCard = ({spinResult, matchScore, setPlayerLetter, playerLetterArray, setSpinResult}) => {
+const PlayerCard = ({spinResult, matchScore, setPlayerLetter, playerLetterArray, animate, setAnimate}) => {
   const [chosenLetter, setChosenLetter] = useState('');
   const { user: currentUser } = useSelector((state) => state.auth);
   const handleChange = (event) => {
@@ -15,19 +15,22 @@ const PlayerCard = ({spinResult, matchScore, setPlayerLetter, playerLetterArray,
   const [isActive, setIsActive] = useState(false);
   const controls = useAnimation();
 
-  const toggleAnimation = () => {
-    console.log('anima')
+  const toggleAnimation = () => { 
     setIsActive(!isActive);
     setTimeout(() => {
-      controls.start({ y: isActive ? 0 : 250 });
+      controls.start({ y: isActive ? 0 : 250 });      
     }, isActive ? 3 * 1000 : 9 * 1000);
+    if(spinResult === 'perdeu'){
+      setTimeout(() => {
+        controls.start({ y: 0 });      
+      }, 12500);
+    }
   };
-
   useEffect(()=>{
     if(spinResult){
       toggleAnimation();
-    }    
-  }, [spinResult])
+    }  
+  }, [animate])
 
   const chooseClickHandler = () => {
       if(!chosenLetter){
@@ -35,10 +38,10 @@ const PlayerCard = ({spinResult, matchScore, setPlayerLetter, playerLetterArray,
       }
       if(playerLetterArray.includes(chosenLetter.toLocaleUpperCase())){
         return
-      } 
-    setPlayerLetter(chosenLetter.toLocaleUpperCase());
-    toggleAnimation();
-    setChosenLetter('');
+      }
+      setAnimate(!animate)
+      setPlayerLetter(chosenLetter.toLocaleUpperCase());
+      setChosenLetter('');
   }
 
   return (
@@ -46,15 +49,14 @@ const PlayerCard = ({spinResult, matchScore, setPlayerLetter, playerLetterArray,
       animate={controls}
       transition={{ duration: 1, ease: "easeInOut"}}
     >
-      <p>Player: {currentUser.username}</p>
+      {spinResult === "perdeu"? "Sinto muito, perdeu tudo!":(<><p>Player: {currentUser.username}</p>
       <div>Score: {matchScore}</div>
       <div>Letras escolhidas: {playerLetterArray}</div>
       <div>Escolha uma letra por: {spinResult}
         <input type="text" disabled={!isActive} value={chosenLetter} 
         onChange={handleChange} maxLength="1"/>
         <button onClick={chooseClickHandler}>Escolher</button>
-      </div>
-    
+      </div></>)}    
     </motion.div>
   )
 }
