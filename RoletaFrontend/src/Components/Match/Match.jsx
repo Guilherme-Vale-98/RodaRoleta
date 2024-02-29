c0ac1e5f09e2068a93929591d7db86f711182900
Guilherme Vale <guilherme.vale98@gmail.com>
2024-02-29 16:13:42 -0300

    add save score and finish match

--- RoletaFrontend/src/Components/Match/Match.jsx
+++ RoletaFrontend/src/Components/Match/Match.jsx
@@ -4,7 +4,7 @@ import WordBoard from "../Board/WordBoard";
 import "./Match.css";
 import { useDispatch, useSelector } from "react-redux";
 import matchService from "../../services/MatchService";
-import { startMatch } from "../../slices/sliceMatch";
+import { startMatch, addScore, saveScore } from "../../slices/sliceMatch";
 import PlayerCard from "../PlayerCard/PlayerCard";
 
 const Match = () => {
@@ -15,74 +15,91 @@ const Match = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [spinResult, setSpinResult] = useState(0);
   const [matchScore, setMatchScore] = useState(0);
-  const [playerLetter, setPlayerLetter] = useState('');
+  const [playerLetter, setPlayerLetter] = useState("");
   const [playerLetterArray, setPlayerLetterArray] = useState([]);
   const dispatch = useDispatch();
-  const [animate, setAnimate]=useState(false);
+  const [animate, setAnimate] = useState(false);
 
   const [finished, setFinished] = useState(false);
-  useEffect(()=>{
+  useEffect(() => {
     setIsLoading(true);
     dispatch(startMatch({ username: currentUser.username }))
-    .unwrap()
-    .then(()=>{
-      setIsLoading(false);
-    })
-    .catch(()=>{
-      setIsLoading(false);
-    });
-  }, [])
+      .unwrap()
+      .then(() => {
+        setIsLoading(false);
+      })
+      .catch(() => {
+        setIsLoading(false);
+      });
+  }, []);
 
-  useEffect(()=>{
-    setPlayerLetterArray(prevArray => [...prevArray, playerLetter]);
+  useEffect(() => {
+    setPlayerLetterArray((prevArray) => [...prevArray, playerLetter]);
+    word && [...word].every((e) => playerLetterArray.includes(e))
+      ? finishMatch()
+      : null;
+  }, [playerLetter]);
 
-    (word && [...word].every(e => playerLetterArray.includes(e)))? setFinished(true) : null;
-  }, [playerLetter])
-
-  
-
-  useEffect(()=>{
-    if(spinResult === "perdeu"){
+  useEffect(() => {
+    if (spinResult === "perdeu") {
       setMatchScore(0);
-      return
+      return;
     }
     let scoreToAdd = 0;
-    [...word].forEach(letter=>{
-      if(letter === playerLetter){
+    [...word].forEach((letter) => {
+      if (letter === playerLetter) {
         scoreToAdd += spinResult;
       }
-    })    
-    setMatchScore(prevScore => prevScore + scoreToAdd);
-    setPlayerLetter(null)
-  }, [playerLetter, spinResult])
+    });
+    setMatchScore((prevScore) => prevScore + scoreToAdd);
+    setPlayerLetter(null);
+  }, [playerLetter, spinResult]);
 
-
-  return (<div>
-    {finished? <div><WordBoard  word={word}
-          playerLetterArray={playerLetterArray}/></div>:renderMatch()}
-  </div>    
+  const finishMatch = () => {
+    dispatch(saveScore({ matchId: match.id, score: matchScore }));
+    setTimeout(() => setFinished(true), 2800);
+  };
+  return (
+    <div>
+      {finished ? (
+        <div>
+          <WordBoard word={word} playerLetterArray={playerLetterArray} />
+        </div>
+      ) : (
+        renderMatch()
+      )}
+    </div>
   );
 
   function renderMatch() {
-    return (isLoading? 'Loading...':<>
-      <div className="match-container">
-        <PlayerCard
-          spinResult={spinResult}
-          matchScore={matchScore}
-          setPlayerLetter={setPlayerLetter}
-          playerLetterArray={playerLetterArray}
-          animate={animate}
-          setAnimate={setAnimate} />
-        <WordBoard
-          word={word}
-          playerLetterArray={playerLetterArray}
-        ></WordBoard>
-        <div className="hint-container">
-          {hint}
+    return isLoading ? (
+      "Loading..."
+    ) : (
+      <>
+        <div className="match-container">
+          <PlayerCard
+            spinResult={spinResult}
+            matchScore={matchScore}
+            setPlayerLetter={setPlayerLetter}
+            playerLetterArray={playerLetterArray}
+            animate={animate}
+            setAnimate={setAnimate}
+          />
+          <WordBoard
+            word={word}
+            playerLetterArray={playerLetterArray}
+          ></WordBoard>
+          <div className="hint-container">{hint}</div>
+          <Wheel
+            setSpinResult={setSpinResult}
+            playerLetter={playerLetter}
+            animate={animate}
+            setAnimate={setAnimate}
+            spinResult={spinResult}
+          />
         </div>
-        <Wheel setSpinResult={setSpinResult} playerLetter={playerLetter} animate={animate} setAnimate={setAnimate} spinResult={spinResult} />
-      </div>
-    </>)
+      </>
+    );
   }
 };
 
